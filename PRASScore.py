91559a09f41fb8dd8090417f21af3ee51b5233d0
DDochtermann <51250343+DDochtermann@users.noreply.github.com>
2020-02-25 20:04:46 -0500

    Add files via upload

--- PRASScore.py
+++ PRASScore.py
@@ -2,7 +2,7 @@ import sys
 
 if __name__ == "__main__":
 	bedFile = "PureCLIP.crosslink_sites"
-	inFile = open(sys.argv[1], 'r')
+	inFile = open("OutData_500nt_PRAS_option_suggestions.txt", 'r')
 	A = inFile.read().splitlines()[1].split("	")
 	outFile = open("ScoreCommand.txt", 'w')
 	outFile.write("python PRAS_1.0.py -g annotation.gtf -t IDs.txt -m score -s %s -i %s.bed -a %s_%s.assign.txt -w 10 -r %s -d %s" % (A[0], bedFile, bedFile, A[0], A[1], A[2]))
